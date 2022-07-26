ead3c8868ccceef3a93ea4f66819bd09719d1ab3
ShadowCoderVM <sergei.semin2010@yandex.ru>
2022-07-27 00:36:27 +0300

    Orders modify tests

--- TradeBot/src/test/java/com/example/TradeBoot/api/services/OrderServicesTest.java
+++ TradeBot/src/test/java/com/example/TradeBoot/api/services/OrderServicesTest.java
@@ -1,52 +1,65 @@
 package com.example.TradeBoot.api.services;
 
 import com.example.TradeBoot.api.domain.markets.ESide;
-import com.example.TradeBoot.api.domain.orders.EType;
+import com.example.TradeBoot.api.domain.orders.Order;
 import com.example.TradeBoot.api.domain.orders.OrderToPlace;
 import com.example.TradeBoot.api.extentions.RequestExcpetions.Checked.BadRequestByFtxException;
 import com.example.TradeBoot.api.http.HttpClientWorker;
-import com.example.TradeBoot.api.http.HttpRequestFactory;
-import com.example.TradeBoot.api.http.HttpResponseHandler;
-import com.example.TradeBoot.configuration.TestConfig;
 import com.example.TradeBoot.configuration.TestServiceInstances;
 import com.example.TradeBoot.configuration.TestUtils;
+import com.example.TradeBoot.trade.calculator.OrderPriceCalculator;
+import com.example.TradeBoot.trade.model.OrderInformation;
+import com.example.TradeBoot.trade.model.Persent;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderServicesTest {
 
-    private static HttpClientWorker httpClient;
     private static OrdersService ordersService;
+
+    private static IMarketService marketService;
+
+    private static OrderPriceCalculator orderPriceCalculator;
+
+    private static HttpClientWorker httpClientWorker;
 
     @BeforeAll
     static void init() {
-        httpClient = TestServiceInstances.getHttpClient();
+        marketService = TestServiceInstances.getMarketService();
         ordersService = TestServiceInstances.getOrdersService();
+        orderPriceCalculator = TestServiceInstances.getOrderPriceCalculator();
+        httpClientWorker = TestServiceInstances.getHttpClient();
     }
 
 
-    public void t(){
+    public void canSet() {
         String marketName = "GMT/USD";
-        var marketService = new IMarketService.Base(httpClient);
 
-        var OrderBook = marketService.getOrderBook(marketName, 20);
-        var bestBid = OrderBook.getBestBid().getPrice();
-        var distance1Percent = new BigDecimal(5);
-        var targetVolume = GetDistanceVolume(bestBid, distance1Percent);
-        System.out.println("Best bid: " + bestBid + "\nDistance percent: " + distance1Percent + "\nTarget Price: " + targetVolume);
-
+        var orderBook = marketService.getOrderBook(marketName, 20);
+        var bestBid = orderBook.getBestBid().getPrice();
+        var persentDistance = new Persent(5);
         var size = 1L;
-        var orderToPlace = new OrderToPlace(marketName, ESide.BUY, targetVolume, EType.LIMIT, BigDecimal.valueOf(size));
-        System.out.println("OrderToPlace " + orderToPlace.toString());
+
+        List<OrderInformation> orderInformations = new ArrayList<>();
+        orderInformations.add(new OrderInformation(BigDecimal.valueOf(size), ESide.BUY, persentDistance));
+
+        var ordersToPlaceMap = orderPriceCalculator.createOrdersToPlaceMap(orderBook, orderInformations, marketName);
+
+        for (OrderToPlace orderToPlace : ordersToPlaceMap.values()) {
+            System.out.println("OrderToPlace " + orderToPlace.toString());
+        }
 
         try {
 
             System.out.println("!!! Start placeOrder !!!");
 
-            System.out.println(ordersService.placeOrder(orderToPlace));
+            for (OrderToPlace orderToPlace : ordersToPlaceMap.values()) {
+                System.out.println(ordersService.placeOrder(orderToPlace));
+            }
 
             var openOrders = ordersService.getOpenOrders(marketName);
 
@@ -68,15 +81,82 @@ public class OrderServicesTest {
         }
         System.out.println("LOG END");
     }
-    private BigDecimal GetDistanceVolume(BigDecimal bestBid, BigDecimal distancePercent) {
-        var value = bestBid
-                .divide(BigDecimal.valueOf(100L), 15, RoundingMode.HALF_UP)
-                .multiply(distancePercent);
 
-        var value2 = bestBid.subtract(value);
-        var scalded = value2.setScale(6, RoundingMode.HALF_UP);
-        return scalded;
+    //не работает хз почему
+    @Test
+    public void canModifyOrder() {
+        String marketName = "GMT/USD";
+
+        var orderBook = marketService.getOrderBook(marketName, 5);
+        var bestBid = orderBook.getBestBid().getPrice();
+        var persentDistance1 = new Persent(5);
+        var persentDistance2 = new Persent(6);
+        var size = 1L;
+
+        List<OrderInformation> orderInformations = new ArrayList<>();
+        orderInformations.add(new OrderInformation(BigDecimal.valueOf(size), ESide.BUY, persentDistance1));
+        orderInformations.add(new OrderInformation(BigDecimal.valueOf(size), ESide.BUY, persentDistance2));
+
+        int placedIndex = 0;
+        int newPriceIndex = 1;
+
+
+        var ordersToPlaceMap = orderPriceCalculator.createOrdersToPlaceMap(orderBook, orderInformations, marketName);
+
+
+
+
+        try {
+            //var clientId = "dfhghfgjhgdfjghjkghuygfj1323124124";
+            System.out.println("!!! Start placeOrder !!!");
+            var toPlaceOrderInformation = orderInformations.get(placedIndex);
+            var orderToPlace = ordersToPlaceMap.get(toPlaceOrderInformation);
+            //orderToPlace.setClientId(clientId);
+            System.out.println("OrderToPlace " + ordersToPlaceMap.get(orderInformations.get(placedIndex)).toString());
+
+
+            var placedOrder = ordersService.placeOrder(orderToPlace);
+
+            var modifyOrderInformation = orderInformations.get(newPriceIndex);
+            var newPrice = ordersToPlaceMap.get(modifyOrderInformation).getPrice();
+
+            modifyOrderPrice1(placedOrder, newPrice);
+            //modifyOrderPrice2(placedOrder, newPrice, clientId);
+
+
+            var openOrders = ordersService.getOpenOrders(marketName);
+            ordersService.cancelAllOrder();
+
+        } catch (BadRequestByFtxException e) {
+            System.out.println(e.getMessage());
+            TestUtils.printOpenOrders(marketName);
+
+            try {
+                ordersService.cancelAllOrder();
+            } catch (BadRequestByFtxException ex) {
+                System.out.println("!!!!!!!!!!!!!!!!!!!Я не могу закрыть ордера!!!!!!!!!!!!!!!!!!!!");
+            }
+
+            TestUtils.printOpenOrders(marketName);
+            System.out.println("!!! All Orders emergency canceled !!!");
+
+        }
+        System.out.println("LOG END");
     }
 
+    private void modifyOrderPrice1(Order order, BigDecimal price) throws BadRequestByFtxException {
+
+        String url = "/orders/" + order.getId() + "/modify";
+        //String body = "{\"price\": " + price + "}";
+        String body = "{\"size\": " + 2 + "}";
+        httpClientWorker.createPostRequest(url, body);
+    }
+
+    private void modifyOrderPrice2(Order order, BigDecimal price, String clientId) throws BadRequestByFtxException {
+        String url = "/orders/by_client_id/" + clientId + "/modify";
+        String body = "{\"size\": " + 2 + "}";
+        httpClientWorker.createPostRequest(url, body);
+
+    }
 
 }
