996e158f70fe967b989f702667638f5a883387f2
ShadowCoderVM <sergei.semin2010@yandex.ru>
2022-08-23 18:10:04 +0300

    error handling of "parse to model exception"

---
+++ TradeBot/src/main/java/com/example/TradeBoot/api/extentions/ParseToModelException.java
@@ -0,0 +1,7 @@
+package com.example.TradeBoot.api.extentions;
+
+public class ParseToModelException extends RuntimeException {
+    public ParseToModelException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
